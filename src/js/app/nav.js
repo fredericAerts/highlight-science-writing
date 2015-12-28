6d377fe4462299c0fe44e395655119e330b78ec6
fredericJos <fredericaerts@hotmail.com>
2015-12-28 10:29:59 +0100

    initial commit

---
+++ src/js/app/nav.js
@@ -0,0 +1,24 @@
+var defaultApp = defaultApp || {};
+
+defaultApp.nav = (($, window, undefined) => {
+
+    var init, testFunction;
+
+
+    init = () => {
+        
+    };
+
+
+    testFunction = (test) => {
+        return () => {
+            
+        };
+    };
+
+    return {
+        init: init
+    };
+
+}(jQuery, window));
+
