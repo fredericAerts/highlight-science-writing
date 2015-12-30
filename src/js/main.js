5b6fd63970cae0d425eb9c2edf453821fbcb87dd
fredericJos <fredericaerts@hotmail.com>
2015-12-30 16:09:15 +0100

    implement basic website

--- src/js/main.js
+++ src/js/main.js
@@ -1,68 +1,68 @@
-var defaultApp = defaultApp || {};
+'use strict';
+var hsr = hsr || {};
 
-defaultApp.app = (($, window, undefined) => {
-
-    let init, initOnload, appScroll, debounce;
-
-
+hsr.app = ((window, undefined) => {
+    
     // On Dom ready
-    init = () => {
-        defaultApp.nav.init();
-        console.log('hello','init');
+    let init = () => {
+        setTimeout(function() {
+            document.querySelector('body').classList.remove('hidden');
+            document.querySelector('.content__logo').classList.add('highlighted');
+        }, 300);
     };
 
 
     // On All loaded
-    initOnload = () => {
-        appScroll();
-    };
+    // initOnload = () => {
+    //     appScroll();
+    // };
 
     // On Scroll
-    appScroll = () => {
-        let _onScroll;
+    // appScroll = () => {
+    //     let _onScroll;
 
-        _onScroll = debounce(() => {
-            console.log('scrolling');
-        }, 50);
+    //     _onScroll = debounce(() => {
+    //         console.log('scrolling');
+    //     }, 50);
 
-        $(window).scroll(() =>{
-            _onScroll();
-        });
-    };
+    //     $(window).scroll(() =>{
+    //         _onScroll();
+    //     });
+    // };
 
-    debounce = (func, wait, immediate) => {
-        let timeout;
-        return (...args) => {
-            let context = this;
-            let later = () => {
-                timeout = null;
-                if (!immediate) {
-                    func.apply(context, args);
-                }
-            };
-            let callNow = immediate && !timeout;
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-            if (callNow) {
-                func.apply(context, args);
-            }
-        };
-    };
+    // debounce = (func, wait, immediate) => {
+    //     let timeout;
+    //     return (...args) => {
+    //         let context = this;
+    //         let later = () => {
+    //             timeout = null;
+    //             if (!immediate) {
+    //                 func.apply(context, args);
+    //             }
+    //         };
+    //         let callNow = immediate && !timeout;
+    //         clearTimeout(timeout);
+    //         timeout = setTimeout(later, wait);
+    //         if (callNow) {
+    //             func.apply(context, args);
+    //         }
+    //     };
+    // };
 
     return {
-        init: init,
-        initOnload: initOnload
+        init: init
+        // initOnload: initOnload
     };
 
-}(jQuery, window));
+}(window));
 
 
 // Dom ready
-$(() => {
-    defaultApp.app.init();
-});
+document.addEventListener('DOMContentLoaded', () => {
+ hsr.app.init();
+}, false);
 
 // All loaded
-$(window).load(() => {
-    defaultApp.app.initOnload();
-});
+// $(window).load(() => {
+//     hsr.app.initOnload();
+// });
